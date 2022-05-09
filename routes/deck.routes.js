1036fc1d12a00fecd83cddc6b5e560164630a2a1
Diogo Carneiro <diogoascarneiro@hotmail.com>
2022-05-09 18:35:59 +0100

    added random deck route with three options for free practice

--- routes/deck.routes.js
+++ routes/deck.routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Deck = require("../models/Deck.model");
+const Card = require("../models/Card.model");
+const User = require("../models/User.model");
 
 router.get("/", async (req, res) => {
   try {
@@ -22,6 +24,27 @@ router.post("/", async (req, res) => {
     const response = await Deck.create({
         name, cards
     });
+    res.status(200).json(response);
+  } catch (e) {
+    res.status(500).json({ message: e });
+  }
+
+});
+router.get("/random", async (req, res) => {
+  const {type, number, userId} = req.query;
+  try {
+    let response;
+    if (type === "random") response = await Card.aggregate([{$sample: {size:Number(number)}}]);
+    if (type === "seenLowFirst") {
+      userResponse = await User.findById(userId).populate("cards.cardId").select("cards -_id");
+      const sortedByLowAvgScore = userResponse.cards.sort((a, b) => a.averageScore - b.averageScore);
+      response = sortedByLowAvgScore.slice(0, number);
+    }
+    if (type === "seenRandom") {
+      userResponse = await User.findById(userId).populate("cards.cardId").select("cards -_id");
+      const shuffledCardList = userResponse.cards.sort(() => 0.5 - Math.random());
+      response = shuffledCardList.slice(0, number);
+    }
     res.status(200).json(response);
   } catch (e) {
     res.status(500).json({ message: e });
@@ -64,5 +87,6 @@ router.put("/:deckId", async (req, res) => {
     res.status(500).json({ message: e });
   }
 });
+
 
 module.exports = router;
